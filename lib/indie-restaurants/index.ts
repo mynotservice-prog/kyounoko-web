981b539ccc418862028ac432eb4a5286182b9f47
kyounoko-author <service@remegift.jp>
2026-05-16 05:18:02 +0000

    feat(restaurants): 駅別個人店ランチ拡充 バッチ（chunk-33・18駅42店）

--- lib/indie-restaurants/index.ts
+++ lib/indie-restaurants/index.ts
@@ -36,6 +36,7 @@ import { CHUNK_29 } from './chunk-29';
 import { CHUNK_30 } from './chunk-30';
 import { CHUNK_31 } from './chunk-31';
 import { CHUNK_32 } from './chunk-32';
+import { CHUNK_33 } from './chunk-33';
 import { CHUNK_KANSAI } from './chunk-kansai';
 import type { StationIndieMap, IndieRestaurant, IndieGenre } from './types';
 
@@ -91,6 +92,7 @@ export const STATION_INDIE_MAP: StationIndieMap = mergeIndieMaps(
   CHUNK_30,
   CHUNK_31,
   CHUNK_32,
+  CHUNK_33,
   CHUNK_KANSAI,
 );
 
